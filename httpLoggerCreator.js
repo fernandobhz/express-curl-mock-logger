63e714d6951cf4d4cfd8992701f427559adb6f04
Fernando Reis Guimaraes <fernando@fernandofrg.com.br>
2024-03-19 23:50:24 -0300

    fix: log to a subfolder with bat, request, response and the file contents

--- httpLoggerCreator.js
+++ httpLoggerCreator.js
@@ -1,25 +1,15 @@
 const fs = require("fs");
+const zlib = require("zlib");
+const path = require('path');
+const os = require('os');
 const { logPathGenerator, ensureDirectoryExists } = require("./helpers.js");
 const { curlCommandGenerator } = require("./curlGenerator.js");
+const { request } = require("http");
 
-const textContentTypesFragments = ["json", "text", "xml", "html", "javascript", "css", "csv", "plain"];
-const unknowFilePrefixDescriptor = `just.response.body`;
+const textContentTypesFragments = ["json", "xml", "html", "javascript", "css", "csv", "plain", "text"];
 
 module.exports = (logFolder = "http-logs", logCurlToConsole = false, logResponseToConsole = false) => {
   return (req, res, next) => {
-    const logPath = logPathGenerator(logFolder);
-    const logReqResPath = `${logPath}.req.res.txt`;
-    ensureDirectoryExists(logPath);
-
-    const curlCommand = curlCommandGenerator(req);
-    const writeStream = fs.createWriteStream(logReqResPath);
-
-    writeStream.write(`${curlCommand}\n\n`);
-
-    if (logCurlToConsole) {
-      log(curlCommand);
-    }
-
     const oldWrite = res.write;
     const oldEnd = res.end;
 
@@ -34,38 +24,85 @@ module.exports = (logFolder = "http-logs", logCurlToConsole = false, logResponse
 
     res.end = function (...args) {
       const [chunk] = args;
-      const bufferChunk = Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk);
-      chunks.push(bufferChunk);
 
-      const chunksBuffer = Buffer.concat(chunks);
-      const body = chunksBuffer.toString("ascii");
-
-      const headers = Object.entries(res.getHeaders())
-        .map(([key, value]) => `${key}: ${value}`)
-        .join("\n");
-
-      const responseContentType = res.get("Content-Type");
-      const fileExtension = responseContentType ? textContentTypesFragments.find((fragment) => responseContentType.includes(fragment)) || "unknow" : "unknow";
-      const isTextResponse = fileExtension !== "unknow";
-
-      const logBodyPath = `${logPath}.${unknowFilePrefixDescriptor}.${fileExtension}`;
-      const logBody = isTextResponse ? body : `Unknow reponse content type, check the '${logBodyPath}' file for its contents`;
-
-      const { httpVersion } = req;
-      const response = `HTTP/${httpVersion} ${res.statusCode}\n${headers}\n\n${logBody}`;
-
-      writeStream.write(response);
-      writeStream.end();
-
-      if (!isTextResponse) {
-        fs.writeFileSync(logBodyPath, chunksBuffer);
-      }
-
-      if (logResponseToConsole) {
-        log(response);
+      if (chunk) {
+        const bufferChunk = Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk);
+        chunks.push(bufferChunk);
       }
 
       oldEnd.apply(res, args);
+
+      /** Logs out the curl and response */
+
+      const responseHeaders = Object.entries(res.getHeaders())
+        .map(([key, value]) => `${key}: ${value}`)
+        .join("\n");
+
+      const requestHeaders = Object.entries(req.headers)
+        .map(([key, value]) => `${key}: ${value}`)
+        .join("\n");      
+
+      const { httpVersion, method: requestMethod, originalUrl, body: requestBody } = req;
+      const responseContentType = res.get("Content-Type");
+      const currentFragment = responseContentType ? textContentTypesFragments.find((fragment) => responseContentType.includes(fragment)) || "unknow" : "unknow";
+      const isTextResponse = currentFragment !== "unknow";
+
+      const urlFileExtension = path.extname(originalUrl.split('?').at(0)).slice(1);
+      const logFileExtension = urlFileExtension || currentFragment;
+
+      const contentEncoding = res.get("Content-Encoding");
+      const isContentEncodedWithGzip = contentEncoding === "gzip";
+      const chunksBuffer = Buffer.concat(chunks);
+
+      const isWindows = os.platform() === 'win32';
+      const logPath = logPathGenerator(logFolder);
+      const logSubFolder = `${logPath}${path.sep}`;
+      const curlLogPath = `${logSubFolder}curl.${isWindows ? 'bat' : 'sh'}`;
+      const requestLogPath = `${logSubFolder}request.txt`;
+      const responseLogPath = `${logSubFolder}response.txt`;
+      const fileContentPath = `${logSubFolder}file.${logFileExtension}`;
+      ensureDirectoryExists(fileContentPath);
+
+      let responseTextBody = "";
+
+      if (isContentEncodedWithGzip && isTextResponse) {
+        responseTextBody = zlib.gunzipSync(chunksBuffer).toString("ascii");
+      } else if (isTextResponse) {
+        responseTextBody = chunksBuffer.toString("ascii");
+      } else {
+        responseTextBody = `Unknow reponse content type, check the full file contents at: ${fileContentPath}`;
+      }
+
+
+      let requestTextBody = "";
+
+      if (typeof requestBody === "object") {
+        requestTextBody = JSON.stringify(requestBody, null, 2);
+
+        if (requestTextBody === '{}') {
+            requestTextBody = '';
+        }
+      } else if (Buffer.isBuffer(requestBody)) {
+        requestTextBody = requestBody.toString("hex");
+      } else {
+        requestTextBody = requestBody || '';
+      }
+  
+      const fullTextRequest = `${requestMethod} ${originalUrl}\n${requestHeaders}\n\n${requestTextBody}`;
+      const fullTextResponse = `HTTP/${httpVersion} ${res.statusCode}\n${responseHeaders}\n\n${responseTextBody}`;
+      const curlCommand = curlCommandGenerator(req);
+
+      fs.writeFileSync(curlLogPath, curlCommand);
+      fs.writeFileSync(requestLogPath, fullTextRequest);
+      fs.writeFileSync(responseLogPath, fullTextResponse);
+      fs.writeFileSync(fileContentPath, chunksBuffer);
+
+
+      if (logCurlToConsole && isTextResponse && logResponseToConsole) {
+        log(curlCommand, `\n`, fullTextResponse);
+      } else if (logCurlToConsole) {
+        log(curlCommand);
+      }
     };
 
     next();
