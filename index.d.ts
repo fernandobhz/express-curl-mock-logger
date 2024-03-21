6896ff71d435bd69ad92fd94652f1a3acb48aa0d
Fernando Reis Guimaraes <fernando@fernandofrg.com.br>
2024-03-20 22:54:09 -0300

    fix: types

--- index.d.ts
+++ index.d.ts
@@ -2,8 +2,8 @@ declare module 'express-curl-mock-logger' {
   import { RequestHandler } from 'express';
 
   function httpLoggerCreator(
-    skipUrlPatterns: Array<RegExp>,
-    logToConsoleWhenSkippingUrls: boolean,
+    skipUrlPatterns?: Array<RegExp>,
+    logToConsoleWhenSkippingUrls?: boolean,
     httpLogsFolderPath?: string,
     logCurlToConsole?: boolean,
     logResponseToConsole?: boolean): RequestHandler;
